f48dc6902dc950908ce029c4c5d58f42e0c3ea6f
ZeM4rtins <73347736+ZeM4artins@users.noreply.github.com>
2022-06-08 12:05:41 +0100

    Tirei extend desnecessário

--- 2º Ano/POO/Exame_2021/src/Utilizador.java
+++ 2º Ano/POO/Exame_2021/src/Utilizador.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Utilizador extends SpotifyPOO {
+public class Utilizador {
 
     private String id; //email
     private String nome;
